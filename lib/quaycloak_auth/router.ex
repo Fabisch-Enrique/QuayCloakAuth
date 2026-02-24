54262f6fe7ca2ec21f12f8bce6f7b0045bbc96a8
Fabisch Enrique <drenriquenicol@gmail.com>
2026-02-24 12:09:19 +0300

    refactor auth routes

--- lib/quaycloak_auth/router.ex
+++ lib/quaycloak_auth/router.ex
@@ -1,13 +1,22 @@
 defmodule QuaycloakAuth.Router do
-  defmacro keycloak_auth_routes(opts \\ []) do
+  defmacro quaycloak_callback_route(opts) do
+    scope = Keyword.get(opts, :scope, "/auth")
+    controller = Keyword.fetch!(opts, :controller)
+
+    quote do
+      scope unquote(scope) do
+        get("/:provider/callback", unquote(controller), :login)
+      end
+    end
+  end
+
+  defmacro quaycloak_logout_route(opts) do
     scope = Keyword.get(opts, :scope, "/")
-    controller = Keyword.get(opts, :controller, QuaycloakAuthWeb.SessionController)
+    controller = Keyword.fetch!(opts, :controller)
 
     quote do
       scope unquote(scope) do
         delete("/logout", unquote(controller), :logout)
-        get("/auth/keycloak", unquote(controller), :request)
-        get("/auth/keycloak/callback", unquote(controller), :login)
       end
     end
   end
