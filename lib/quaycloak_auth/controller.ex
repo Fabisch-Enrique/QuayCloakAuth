07461accbd06b3933c3f3c34d2ea1cb621173e44
Fabisch Enrique <drenriquenicol@gmail.com>
2026-02-25 18:41:33 +0300

    fix controller inclusion

--- lib/quaycloak_auth/controller.ex
+++ lib/quaycloak_auth/controller.ex
@@ -1,61 +1,76 @@
 defmodule QuaycloakAuth.Controller do
   @moduledoc false
 
-  # This module contains the actions; host controller will `use` it.
-  defmacro __using__(_opts) do
+  use Phoenix.Controller, formats: [:html]
+
+  require Logger
+
+  defmacro __using__(opts) do
+    app_name = Keyword.fetch!(opts, :otp_app)
+
     quote do
-      use Phoenix.Controller, formats: [:html]
+      # Keep action signatures normal: (conn, params)
+      def request(conn, params),
+        do: request(conn, params, unquote(app_name))
 
-      require Logger
+      def login(conn, params), do: login(conn, params, unquote(app_name))
 
-      def request(conn, _params), do: conn
-
-      def login(
-            %{assigns: %{ueberauth_failure: %Ueberauth.Failure{errors: errors}}} = conn,
-            %{"app_name" => app_name} = _params
-          ) do
-        message = errors |> Enum.map(& &1.message) |> Enum.join(", ")
-
-        """
-        OAUTH FAILURE
-
-        AUTHENTICATION FAILED with REASON:: #{message}
-        """
-        |> Logger.warning()
-
-        conn
-        |> put_flash(:error, "Authentication Failed REASON: #{message}")
-        |> redirect(to: QuaycloakAuth.routes(app_name).login_path)
-      end
-
-      def login(%{assigns: %{ueberauth_auth: auth}} = conn, %{"app_name" => app_name}) do
-        callbacks = QuaycloakAuth.callbacks(app_name)
-
-        """
-        OAUTH SUCCESS
-
-        AUTHENTICATION SUCCESSFUL...
-        """
-        |> Logger.debug()
-
-        with {:ok, user_info, raw_info} <- QuaycloakAuth.Ueberauth.extract(auth),
-             {:ok, conn} <- callbacks.login(conn, user_info, raw_info) do
-          redirect(conn, to: QuaycloakAuth.routes(app_name).redirect_uri)
-        else
-          {:error, reason} ->
-            Logger.warning("Login failed with REASON:: #{inspect(reason)}")
-
-            conn
-            |> put_flash(:error, "Login Failed")
-            |> redirect(to: QuaycloakAuth.routes(app_name).login_path)
-        end
-      end
-
-      def logout(conn, %{"app_name" => app_name}) do
-        conn
-        |> QuaycloakAuth.callbacks(app_name).logout()
-        |> redirect(to: QuaycloakAuth.routes(app_name).logout_path)
-      end
+      def logout(conn, params),
+        do: logout(conn, params, unquote(app_name))
     end
+  end
+
+  def request(conn, _params, _app_name), do: conn
+
+  def login(
+        %{assigns: %{ueberauth_failure: %Ueberauth.Failure{errors: errors}}} = conn,
+        _params,
+        app_name
+      ) do
+    message = errors |> Enum.map(& &1.message) |> Enum.join(", ")
+
+    Logger.warning("""
+    OAUTH FAILURE
+
+    AUTHENTICATION FAILED with REASON:: #{message} for #{app_name} APP
+    """)
+
+    conn
+    |> put_flash(:error, "Authentication Failed REASON: #{message}")
+    |> redirect(to: QuaycloakAuth.routes(app_name).login_path)
+  end
+
+  def login(%{assigns: %{ueberauth_auth: auth}} = conn, _params, app_name) do
+    callbacks = QuaycloakAuth.callbacks(app_name)
+
+    Logger.debug("""
+    OAUTH SUCCESS
+
+    AUTHENTICATION SUCCESSFUL...
+    """)
+
+    with {:ok, user_info, raw_info} <- QuaycloakAuth.Ueberauth.extract(auth),
+         {:ok, conn} <- callbacks.login(conn, user_info, raw_info) do
+      redirect(conn, to: QuaycloakAuth.routes(app_name).redirect_uri)
+    else
+      {:error, reason} ->
+        Logger.warning("Login failed with REASON:: #{inspect(reason)}")
+
+        conn
+        |> put_flash(:error, "Login Failed")
+        |> redirect(to: QuaycloakAuth.routes(app_name).login_path)
+    end
+  end
+
+  def login(conn, _params, app_name) do
+    conn
+    |> put_flash(:error, "Login Failed")
+    |> redirect(to: QuaycloakAuth.routes(app_name).login_path)
+  end
+
+  def logout(conn, _params, app_name) do
+    conn
+    |> QuaycloakAuth.callbacks(app_name).logout()
+    |> redirect(to: QuaycloakAuth.routes(app_name).logout_path)
   end
 end
